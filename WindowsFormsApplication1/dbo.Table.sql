d4b9834fc83e0c88e6be507c25f8b55b1dd95cd0
bondarenkokate73 <39091843+bondarenkokate73@users.noreply.github.com>
2018-06-05 19:46:16 +0400

    zadanie4

---
+++ WindowsFormsApplication1/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Date] DATE NOT NULL, 
+    [Player1] NCHAR(10) NULL DEFAULT PlayerOne, 
+    [Player2] NCHAR(10) NULL DEFAULT PlayerTwo, 
+    [KolvoHod] INT NOT NULL, 
+    [Win] INT NOT NULL
+)
---
+++ WindowsFormsApplication1/БД.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class БД
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Player1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Player2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.KolvoHod = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Win = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.Date,
+            this.Player1,
+            this.Player2,
+            this.KolvoHod,
+            this.Win});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(927, 450);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // id
+            // 
+            this.id.HeaderText = "Номер игры";
+            this.id.MinimumWidth = 2;
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // Date
+            // 
+            this.Date.HeaderText = "Дата";
+            this.Date.MinimumWidth = 7;
+            this.Date.Name = "Date";
+            this.Date.ReadOnly = true;
+            this.Date.Width = 120;
+            // 
+            // Player1
+            // 
+            this.Player1.HeaderText = "Игрок 1";
+            this.Player1.Name = "Player1";
+            this.Player1.ReadOnly = true;
+            // 
+            // Player2
+            // 
+            this.Player2.HeaderText = "Игрок 2";
+            this.Player2.Name = "Player2";
+            this.Player2.ReadOnly = true;
+            // 
+            // KolvoHod
+            // 
+            this.KolvoHod.HeaderText = "Кол-во ходов";
+            this.KolvoHod.MinimumWidth = 3;
+            this.KolvoHod.Name = "KolvoHod";
+            this.KolvoHod.ReadOnly = true;
+            // 
+            // Win
+            // 
+            this.Win.HeaderText = "Выиграл";
+            this.Win.MinimumWidth = 3;
+            this.Win.Name = "Win";
+            this.Win.ReadOnly = true;
+            // 
+            // БД
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "БД";
+            this.Text = "БД";
+            this.Load += new System.EventHandler(this.БД_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Player1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Player2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn KolvoHod;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Win;
+    }
+}
